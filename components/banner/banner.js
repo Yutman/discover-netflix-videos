1c4d57a7218ccfa476be83309ecc1cff4e025803
Peter Kuria <kuriapeter95@gmail.com>
2025-03-12 15:06:55 +0300

    correct backgroundImage URL syntax in Banner component

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styles from './banner.module.css';
 
-const Banner = () => {
- return (
-  <div>
-    Hi banner
-  </div>
-);
-}
+const Banner = (props) => {
+    const { title, subTitle, imgUrl } = props;
+    
+    const handleOnPlay = () => {
+        console.log('handleOnPlay');
+    };
+   
+    return (
+        <div className={styles.container}>
+            <div className={styles.leftWrapper}>
+                <div className={styles.left}>
+                    <div className={styles.nseriesWrapper}>
+                        <p className={styles.firstLetter}>N</p>
+                        <p className={styles.series}>S E R I E S</p>
+                    </div>
+                    <h3 className={styles.title}>{title}</h3>
+                    <h3 className={styles.subTitle}>{subTitle}</h3> 
+
+                    <div className={styles.playBtnWrapper}>
+                        <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+                            <span className={styles.playText}>Play</span>
+                            </button>
+                    </div>
+                </div>
+            </div>
+            <div 
+                className={styles.bannerImg}
+                style={{
+                    backgroundImage: `url(${imgUrl})`, // Fixed the syntax here
+                    width: '100%', 
+                    height: '100%',
+                    position: 'absolute',
+                    backgroundSize: 'cover',
+                    backgroundPosition: '50% 50%', 
+                }}
+            >
+            </div>
+        </div>
+    );
+};
 
 export default Banner;
+
