88ad5e1c3706692a58a14ceae972af14eb66c41c
Peter Kuria <kuriapeter95@gmail.com>
2025-03-13 14:20:57 +0300

    Add navbar dropdown on user account

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 import styles from './navbar.module.css';
 import {useRouter} from 'next/router';
 import Link from 'next/link';
@@ -8,14 +9,23 @@ const NavBar = (props) => {
     const {username} = props;
     const router = useRouter();
 
+    const [showDropdown, setShowDropdown] = useState(false);
+
     const handleOnClickHome = (e) => {
     e.preventDefault();
     router.push('/');
-    }
+    };
+
     const handleOnClickMyList = (e) => {
     e.preventDefault();
     router.push('/browse/my-list');
+    };
+
+    const handleShowDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown(!showDropdown);
     }
+
 
   return ( 
     <div className={styles.container}>
@@ -33,12 +43,15 @@ const NavBar = (props) => {
 
     <nav className={styles.navContainer}>
         <div>
-            <button className={styles.usernameBtn}>
+            <button className={styles.usernameBtn}
+            onClick={handleShowDropdown}>
             <p className={styles.username}>
                 {username}
             </p>
             {/** Expand more icon**/}
             </button>
+
+            {showDropdown && (
             <div className={styles.navDropdown}>
                 <div>
                 <Link legacyBehavior href='/login'>
@@ -47,6 +60,7 @@ const NavBar = (props) => {
                 <div className={styles.lineWrapper}></div>
                 </div>
             </div>
+            )}
         </div>
      </nav>
       </div>
