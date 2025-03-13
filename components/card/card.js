a98cbfbcc0ab9d0453a132eadf2d4f3a0aab9ccb
Peter Kuria <kuriapeter95@gmail.com>
2025-03-13 16:07:09 +0300

    add dynamic class mapping for card sizes and implement responsive Image component

--- components/card/card.js
+++ components/card/card.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import Image from 'next/image';
+import styles from './card.module.css';
 
 const Card = (props) => {
-
     const {imgUrl, size} = props;
 
+    const classMap = {
+        'large': styles.lgItem,
+        'medium': styles.mdItem,
+        'small': styles.smItem,
+    };
+
   return ( 
-  <div> 
+  <div className={styles.container}> 
     Card
+    <div className={classMap[size]}>
     <Image 
         src={imgUrl} 
         alt='image'
-        width={300}
-        height={200}/> 
+        layout='fill'
+        className={styles.cardImg}
+    /> 
+    </div>
     </div>
   )
 };
