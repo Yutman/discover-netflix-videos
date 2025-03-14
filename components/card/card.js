6374cdf2d45d113d0009bf184f86e251d2904343
Peter Kuria <kuriapeter95@gmail.com>
2025-03-14 14:37:26 +0300

    add error handling for image loading in card component

--- components/card/card.js
+++ components/card/card.js
@@ -1,24 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from './card.module.css';
 
 const Card = (props) => {
-    const {imgUrl, size} = props;
+    const {imgUrl = '/static/images/shawshank_redemption.jpg', 
+            size='medium'} = props;
 
+    
+    const [imgSrc, setImgSrc]=useState(imgUrl);
     const classMap = {
-        'large': styles.lgItem,
-        'medium': styles.mdItem,
-        'small': styles.smItem,
+        large: styles.lgItem,
+        medium: styles.mdItem,
+        small: styles.smItem,
     };
+
+    const handleOnError = () => {
+        console.log('Hii Error');
+        setImgSrc('/static/images/shawshank_redemption.jpg')
+    }
 
   return ( 
   <div className={styles.container}> 
     Card
     <div className={classMap[size]}>
     <Image 
-        src={imgUrl} 
+        src={imgSrc} 
         alt='image'
         layout='fill'
+        onError={handleOnError}
         className={styles.cardImg}
     /> 
     </div>
