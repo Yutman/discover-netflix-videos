052c5a804ddfea6bb4b8ddbc674cff251608fa29
Peter Kuria <kuriapeter95@gmail.com>
2025-03-14 17:12:29 +0300

    create SectionCards component to display card with title

---
+++ components/card/section-cards.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './card';
+import styles from './section-cards.module.css';
+
+const SectionCards = (props) => {
+
+    const {title} = props;
+
+  return (
+  <section className={styles.container}>
+           <h2 className={styles.title}>{title}</h2>
+           <div className={styles.cardWrapper}>
+        <Card imgUrl='/static/images/shawshank_redemption.jpg'
+                size='large'/>
+            </div> 
+    </section>
+  )
+};
+
+export default SectionCards;
