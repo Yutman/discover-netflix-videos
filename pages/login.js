1b286980fcc08e269ca210fd335175e79f1742b6
Peter Kuria <kuriapeter95@gmail.com>
2025-03-24 16:40:52 +0300

    setup of signin page

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Login = () => {
+  return (
+  <div>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+    </div>
+  ); 
+};
+
+export default Login
