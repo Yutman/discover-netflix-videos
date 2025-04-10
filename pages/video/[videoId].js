5873cc9bf366896e2d9b8382d9b3594b8f6aedcf
Peter Kuria <kuriapeter95@gmail.com>
2025-04-10 12:32:37 +0300

    Add Modal component to [videoId].js for video display with overlay styling and content

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,10 +1,23 @@
 import {useRouter} from "next/router";
+import styles from "../../styles/Video.module.css";
+import Modal from "react-modal";
+
+Modal.setAppElement("#__next"); // Set the app element for screen readers
 
 const Video = ()=> {
     const router = useRouter();
     console.log({router});
     
-    return <div>Video Page{router.query.videoId}</div>;
+    return <div>Video Page{router.query.videoId}
+    <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={()=> {}}
+        overlayClassName={styles.overlay}
+      >
+      <div>Modal Body</div>
+      </Modal>
+      </div>;
 };
 
 export default Video;
