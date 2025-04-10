1b51b2a421b479b123f19f043a29ff988737c25c
Peter Kuria <kuriapeter95@gmail.com>
2025-04-10 14:27:49 +0300

    implemement youtube api player and docs

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -8,16 +8,26 @@ const Video = ()=> {
     const router = useRouter();
     console.log({router});
     
-    return <div>Video Page{router.query.videoId}
+    return (
+    <div className={styles.container}>
     <Modal
         isOpen={true}
         contentLabel="Watch the video"
         onRequestClose={()=> router.back()}
+        className={styles.modal}
         overlayClassName={styles.overlay}
       >
-      <div>Modal Body</div>
+        <iframe 
+          id="ytplayer" 
+          type="text/html" 
+          width="640" 
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          frameborder="0"
+          ></iframe>
       </Modal>
-      </div>;
+      </div>
+    );
 };
 
 export default Video;
