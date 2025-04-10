b858184732056b8f2c207f7e6e2c2762b86d76c6
Peter Kuria <kuriapeter95@gmail.com>
2025-04-10 16:56:38 +0300

    Add video details to modal display with dynamic content

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -2,12 +2,22 @@ import {useRouter} from "next/router";
 import styles from "../../styles/Video.module.css";
 import Modal from "react-modal";
 
+import clsx from "classnames";
+
 Modal.setAppElement("#__next"); // Set the app element for screen readers
 
 const Video = ()=> {
     const router = useRouter();
-    console.log({router});
+
+    const video = {
+      title: 'Hi',
+      publishTime: '1994-09-23',
+      description: 'A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion',
+      channelTitle: 'Warners Brothers',
+      viewCount: '10000', 
+    }
     
+    const {title, publishTime, description, channelTitle, viewCount} = video;
     return (
     <div className={styles.container}>
     <Modal
@@ -19,12 +29,43 @@ const Video = ()=> {
       >
         <iframe 
           id="ytplayer" 
+          className={styles.videoPlayer}
           type="text/html" 
-          width="640" 
+          width="100%" 
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameborder="0"
           ></iframe>
+
+          <div className={styles.modalBody}>
+            <div className={styles.modalBodyContent}>
+                <div className={styles.col1}>
+                  <p className={styles.publishTime}>
+                    {publishTime}
+                  </p>
+                   <p className={styles.title}>
+                    {title}
+                  </p>
+                   <p className={styles.description}>
+                    {description}
+                  </p>
+                </div>
+                 <div className={styles.col2}>
+                   <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                    <span className={styles.textColor}>Cast: </span>
+                     <span className={styles.channelTitle}>
+                      {channelTitle}
+                     </span>
+                  </p>
+                    <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                    <span className={styles.textColor}>View Count: </span>
+                     <span className={styles.channelTitle}>
+                      {viewCount}
+                     </span>
+                  </p>
+                 </div>
+            </div>
+          </div>
       </Modal>
       </div>
     );
