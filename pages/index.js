b4a4800f9fc4c9bc713e395639842a6ab8341389
Peter Kuria <kuriapeter95@gmail.com>
2025-04-25 14:54:36 +0300

    implement query live Hasura server with env variables

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import NavBar from "../components/nav/navbar";
 import SectionCards from "../components/card/section-cards";
 
 import {getPopularVideos, getVideos} from '../lib/videos';
-import {startFetchMyQuery} from '../lib/db/hasura';
+
 
 export async function getServerSideProps() {
     const disneyVideos = await getVideos
